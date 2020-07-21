5097c3a5a3c611694f951821df5b1ff22aa66026
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-07-21 14:12:08 -0400

    gestion des mutli

--- nav.js
+++ nav.js
@@ -1,4 +1,19 @@
 window.onload = function () {
+
+  // Evenement ajout
+  let plus = document.getElementsByClassName("plus");
+  [].forEach.call(plus, (el) => {
+    el.addEventListener("click", function (e) {
+      let parent_tab =  e.target.parentElement.parentElement.parentElement;
+      let container = parent_tab.getElementsByClassName("container-instance")[0];
+
+      container.innerHTML += container.innerHTML;
+    })
+  });
+
+  // Prendre tous les noeuds apres le h2 et les mettre dans un container
+
+
   var myTabs = tabs({
     el: '#navigation',
     tabNavigationLinks: '.c-tabs-nav__link',
