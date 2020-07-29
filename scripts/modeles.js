3bdd0b8917b83386438408b0bd43814c735550d6
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-07-29 16:25:38 -0400

    copie des modeles

---
+++ scripts/modeles.js
@@ -0,0 +1,106 @@
+var modeles = {
+
+  date_1colonne: `
+<div class="question date_1colonne">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="date">
+</div>
+  `,
+  num_1colonne: ` <div class="question num_1colonne">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="number">
+</div> ` ,
+  text_1colonne: `
+<div class="question text_1colonne">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="text" placeholder="<%= question.placeholder %>">
+</div>
+  `,
+  cases_et_texte: `
+<div class="question cases_et_texte">
+  <% question.options.forEach((option) => { %>
+    <p>
+    <input type="checkbox" id="<%= question.id + "-" + option.id  + (multiples ? "-1" : "") %>" name="<%= question.id + "-" + option.id + (multiples ? "-1" : "") %>" value="<%= option.id %>">
+    <label for="<%= question.id + "-" + option.id + (multiples ? "-1" : "") %>"><%= option.nom_commun + " (" + option.noms_rue + ")" %></label>
+    </p>
+  <% }) %>
+</div>
+  `,
+  deroulant_1colonne: `
+<div class="question deroulant_1colonne">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <select id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>">
+    <% question.options.forEach((option) => { %>
+        <option value="<%= option.toLowerCase() %>"><%= option %></option>
+    <% }) %>
+  </select>
+</div>
+  `,
+  image_1colonne: `
+<div class="question image_1colonne">
+  <i class="fas fa-times-circle fa-2x" style="display: none;" onclick="delete_image(this);"></i> 
+  <label for="<%= question.id + (multiples ? "-1" : "") %>" style="margin-right: auto; padding-left: 10px;"><%= question.nom %></label>
+  <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="file" onchange="readURL(this);">
+  <img id="<%= question.id + (multiples ? "-1" : "") %>-display" src="#" style="display: none;">
+</div>
+  `,
+  num_deroulant_2colonnes: `
+<div class="question num_deroulant_2colonnes">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <div class="valeurs">
+    <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="number">
+    <select id="<%= question.id + (multiples ? "-1" : "") %>-option" name="<%= question.id + (multiples ? "-1" : "") %>-option">
+      <% question.options.forEach((option) => { %>
+          <option value="<%= option.toLowerCase() %>"><%= option %></option>
+      <% }) %>
+    </select>
+  </div>
+</div>
+  `,
+  num_plusieurs_colonnes: `
+<div class="question num_plusieurs_colonnes">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="text" placeholder="<%= question.placeholder %>">
+</div>
+  `,
+  sous_section: `
+<div class="sous-section">
+  <h3><%= question.nom %></h3>
+</div>
+  `,
+  tab_content: `
+<div id="<%= section.id %>" class="c-tab">
+  <div class="c-tab__content">
+    <div class="entete-tab">
+      <h2> <%= section.nom %> </h2>
+      <% if (section.multiples) { %>
+        <div class="liste-tab">
+
+        </div>
+        <div class="plus">
+          <i class="fas fa-plus"></i>
+        </div>
+      <% } %>
+    </div>
+    <div class="container-instance">
+      <p><%= section.description %></p>
+      <% section.questions.forEach((question) => { %>
+          <%- include(process.env.PWD + '/views/partials/' + question.affichage + '.ejs', { question: question, multiples: section.multiples }) %>
+      <% }) %>
+    </div>
+  </div>
+</div>
+  `,
+  textarea_1colonne: `
+<div class="question textarea_1colonne">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <textarea id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" placeholder="<%= question.placeholder %>"></textarea>
+</div>
+  `,
+  text_2colonnes: `
+<div class="question text_2colonnes">
+  <label for="<%= question.id + (multiples ? "-1" : "") %>"><%= question.nom %></label>
+  <input id="<%= question.id + (multiples ? "-1" : "") %>" name="<%= question.id + (multiples ? "-1" : "") %>" type="text" placeholder="<%= question.placeholder %>">
+</div>
+  `
+}
---
+++ scripts/navigation.js
@@ -0,0 +1,115 @@
+window.CONFIG = {
+  suspects: {
+    actuel: 1,
+    max: 5
+  },
+  vehicules: {
+    actuel: 1,
+    max: 5
+  },
+  images: {
+    actuel: 1,
+    max: 5
+  }
+}
+
+window.onload = function () {
+
+  // Evenement ajout
+  let plus = document.getElementsByClassName("plus");
+  [].forEach.call(plus, (el) => {
+    el.addEventListener("click", function (e) {
+      let parent_tab =  e.target.parentElement.parentElement.parentElement;
+      let container = parent_tab.getElementsByClassName("container-instance")[0];
+
+      let id_section = parent_tab.parentElement.id;
+      let config_section = window.CONFIG[id_section];
+
+      // Verifier max
+      if(config_section.actuel >= config_section.max) return;
+
+      // Integrer la nouvelle section
+      doubler_section(config_section.actuel, container);
+
+      // Avancer le compteur
+      config_section.actuel += 1;
+    })
+  });
+
+  var myTabs = tabs({
+    el: '#navigation',
+    tabNavigationLinks: '.c-tabs-nav__link',
+    tabContentContainers: '.c-tab'
+  });
+
+  myTabs.init();
+
+  // WTF ...
+  myTabs.goToTab(1);
+  myTabs.goToTab(0);
+}
+
+function doubler_section (index, container) {
+
+  // Copier
+  let contenu = document.createElement("div");
+  contenu.className = "container-instance";
+  contenu.innerHTML = container.innerHTML;
+
+  // Mettre a jour les ids
+  contenu.innerHTML.replace(/(\w+-)(1)(")/gm, "$1" + index + "$3");
+  let inputs = contenu.getElementsByTagName("input");
+  let textareas = contenu.getElementsByTagName("textarea");
+  let imgs = contenu.getElementsByTagName("img");
+
+  // Reset les valeurs
+  [].forEach.call(inputs, (el) => {
+    el.value = "";
+    el.style.display = "initial";
+  });
+
+  [].forEach.call(textareas, (el) => {
+    el.innerHTML = "";
+  });
+
+  [].forEach.call(imgs, (el) => {
+    el.src = "";
+    el.style.display = "none";
+  });
+
+  container.parentElement.appendChild(contenu);
+}
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      let image = input.nextElementSibling;
+      let icone_delete = input.previousElementSibling.previousElementSibling;
+      image.src = e.target.result;
+
+      toggle(input);
+      toggle(image);
+      toggle(icone_delete);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function toggle (element) {
+  if(element.style.display == "none") {
+    element.style.display = "initial";
+  }
+  else {
+    element.style.display = "none";
+  }
+}
+
+function delete_image(element) {
+  element.parentElement.parentElement.removeChild(element.parentElement);
+}
+
+function test () {
+  console.log(ejs.render(modeles.num_1colonne, { question: { id: 1 }, multiples: false }))
+}
